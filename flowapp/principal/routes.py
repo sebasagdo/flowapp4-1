3b010e325dbe8e77a608baac6614b2594e2f1476
Juan Guillermo <jeissonruiz27@gmail.com>
2019-04-21 13:57:30 -0500

    Inclusión Estado EnLinea/No Enlinea

--- flowapp/principal/routes.py
+++ flowapp/principal/routes.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, render_template, url_for, flash, redirect, request, abort
 from flask_login import current_user
 from flowapp.models import UserDevice, DeviceConsumption
-from datetime import datetime, timedelta
+from datetime import datetime,timedelta
 
 
 principal = Blueprint('principal', __name__)
@@ -15,12 +15,16 @@ def home():
         page = request.args.get('page', 1, type=int)
         devices = UserDevice.query.filter_by(idUserFK=current_user.id).order_by(
             UserDevice.linkDate.desc()).paginate(page=page, per_page=5)
-        #listConsumos = DeviceConsumption.query.filter_by(
-            #idUserDevice=49).order_by(DeviceConsumption.date.desc())
-        return render_template('home.html', devices=devices, datetime=datetime)
+        return render_template('home.html', devices=devices, datetime=datetime, compare=compare)
     return render_template('about.html', title='Acerca de')
 
 
 @principal.route("/about")
 def about():
     return render_template('about.html', title='Acerca de')
+
+
+def compare(dateUltimoConsumo):
+    if (datetime.now()<dateUltimoConsumo + timedelta(minutes=5)):
+        return True
+    return False
